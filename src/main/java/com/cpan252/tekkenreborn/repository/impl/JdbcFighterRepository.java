3313b6b18d87fdcf3a985a034f7f0e2c343a9ed5
chagio623 <alexthan98@gmail.com>
2023-03-01 23:34:40 -0500

    l5

--- src/main/java/com/cpan252/tekkenreborn/repository/impl/JdbcFighterRepository.java
+++ src/main/java/com/cpan252/tekkenreborn/repository/impl/JdbcFighterRepository.java
@@ -1,53 +1,48 @@
 package com.cpan252.tekkenreborn.repository.impl;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.cpan252.tekkenreborn.model.Fighter;
+import com.cpan252.tekkenreborn.model.Fighter.Anime;
 import com.cpan252.tekkenreborn.repository.FighterRepository;
-
-//There's an acronym called CRUD that stands for Create, Read, Update, Delete
-//This is a repository that implements the CRUD operations for fighter table
 
 @Repository
 public class JdbcFighterRepository implements FighterRepository {
-    // We could use @Autowired annotation here but it's not recommended
-    // This approach is more testable and more recommended than using @Autowired
+
     private JdbcTemplate jdbcTemplate;
 
-    // JdbcTemplate is a bean provided by Spring
-    // if jdbctemplate.query() is called, you can give me this result set
     public JdbcFighterRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Iterable<Fighter> findAll() {
-        /**
-         * 1. We query the fighter table for all the fighters
-         * 2. We iterate each row returned from the table
-         * 3. We create a Fighter object from each row (map row to Fighter object)
-         * Optional: we can provide RowMapper that will be responsible for mapping the
-         * row to Fighter object
-         */
-        return jdbcTemplate.query("SELECT * FROM fighter", new FighterRowMapper());
+        String query = "SELECT * FROM fighter";
+        RowMapper<Fighter> rowMapper = new FighterRowMapper();
+        return jdbcTemplate.query(query, rowMapper);
     }
 
     @Override
     public Optional<Fighter> findById(Long id) {
-        Fighter fighter = jdbcTemplate.queryForObject("SELECT * FROM fighter WHERE id = ?", new FighterRowMapper(), id);
+        String query = "SELECT * FROM fighter WHERE id = ?";
+        RowMapper<Fighter> rowMapper = new FighterRowMapper();
+        Fighter fighter = jdbcTemplate.queryForObject(query, rowMapper, id);
         return Optional.ofNullable(fighter);
     }
 
     @Override
-    public void save(Fighter fighter) {
-        var insertFighter = "INSERT INTO fighter (name, damage_per_hit, health, resistance, anime_from, created_at) VALUES (?, ?, ?, ?, ?, ?)";
-        var keyHolder = new GeneratedKeyHolder();
+    public <S extends Fighter> S save(S fighter) {
+        String query = "INSERT INTO fighter (name, damage_per_hit, health, resistance, anime_from, created_at) VALUES (?, ?, ?, ?, ?, ?)";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
-            var ps = connection.prepareStatement(insertFighter);
+            var ps = connection.prepareStatement(query, new String[] { "id" });
             ps.setString(1, fighter.getName());
             ps.setInt(2, fighter.getDamagePerHit());
             ps.setInt(3, fighter.getHealth());
@@ -56,6 +51,71 @@ public class JdbcFighterRepository implements FighterRepository {
             ps.setString(6, fighter.getCreatedAt().toString());
             return ps;
         }, keyHolder);
+
+        Long id = (Long) keyHolder.getKey();
+        fighter.setId(id);
+
+        return fighter;
     }
 
+    @Override
+    public <S extends Fighter> Iterable<S> saveAll(Iterable<S> entities) {
+        throw new UnsupportedOperationException("Unimplemented method 'saveAll'");
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'existsById'");
+    }
+
+    @Override
+    public Iterable<Fighter> findAllById(Iterable<Long> ids) {
+        throw new UnsupportedOperationException("Unimplemented method 'findAllById'");
+    }
+
+    @Override
+    public long count() {
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public void delete(Fighter entity) {
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> ids) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllById'");
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Fighter> entities) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+
+    @Override
+    public void deleteAll() {
+        String query = "DELETE FROM fighter";
+        jdbcTemplate.update(query);
+    }
+
+    @Override
+    public List<Fighter> findByAnimeFrom(Anime anime) {
+        String query = "SELECT * FROM fighter WHERE anime_from = ?";
+        RowMapper<Fighter> rowMapper = new FighterRowMapper();
+        return jdbcTemplate.query(query, rowMapper, anime.name());
+    }
+
+    @Override
+    public List<Fighter> findByNameStartsWithAndCreatedAtBetween(String name, LocalDate startDate, LocalDate endDate) {
+        String query = "SELECT * FROM fighter WHERE name LIKE ? AND created_at BETWEEN ? AND ?";
+        RowMapper<Fighter> rowMapper = new FighterRowMapper();
+        return jdbcTemplate.query(query, rowMapper, name + "%", startDate, endDate);
+    }
 }
